df871616e2495048491f90a055bdb4f774b00be1
ColineVL <coline.vanleeuwen@gmail.com>
2021-12-16 15:04:55 +0100

    enlevé code mort

--- periodic_task.c
+++ periodic_task.c
@@ -2,29 +2,26 @@
 #include <semaphore.h>
 #include <stdlib.h>
 
-#include "periodic_task.h" 
+#include "periodic_task.h"
 #include "utils.h"
 
 /*****************************************************************************/
-void *periodic_task_body (void *parameters);
+void *periodic_task_body(void *parameters);
 
-void *periodic_task_body (void *parameters) {
-  struct timespec trigger;                      /* Stores next dispatch time */
-  struct timespec period;                              /* Period of the task */
+void *periodic_task_body(void *parameters)
+{
+  struct timespec trigger; /* Stores next dispatch time */
+  struct timespec period;  /* Period of the task */
   sem_t timer;
-  task_parameters *my_parameters = (task_parameters *) parameters;
+  task_parameters *my_parameters = (task_parameters *)parameters;
 
   period = (*my_parameters).period;
 
-  //sem_t semaphore;
-  //int sem_res = 0;
   sem_init(&timer, 0, 0);
   clock_gettime(CLOCK_REALTIME, &trigger);
-  //sem_res = sem_timedwait(&semaphore, &(parameters->period));
-  //if(sem_res == -1) {
-  //  job()
-  //}
-  for(;;) {
+
+  for (;;)
+  {
     (*my_parameters).job();
     add_timespec(&trigger, &trigger, &period);
     sem_timedwait(&timer, &trigger);
@@ -32,30 +29,34 @@ void *periodic_task_body (void *parameters) {
 }
 
 /*****************************************************************************/
-void create_periodic_task (struct timespec period, void (*job) (void)) {
+void create_periodic_task(struct timespec period, void (*job)(void))
+{
   pthread_t tid;
-  task_parameters *parameters = malloc (sizeof (task_parameters));
+  task_parameters *parameters = malloc(sizeof(task_parameters));
 
   parameters->period = period;
   parameters->job = job;
-  
+
   pthread_create(&tid, NULL, periodic_task_body, parameters);
 }
 
 /*****************************************************************************/
 #ifdef __TEST__
-void dummy (void) {
-  printf ("o< \n");
+void dummy(void)
+{
+  printf("o< \n");
 }
 
-int main (void) {
+int main(void)
+{
   struct timespec period;
   period.tv_nsec = 250 * 1000 * 1000;
-  period.tv_sec  = 0 ;
+  period.tv_sec = 0;
 
-  create_periodic_task (period, dummy);
-  printf ("Task created\n");
-  while (1);
+  create_periodic_task(period, dummy);
+  printf("Task created\n");
+  while (1)
+    ;
   return 0;
 }
 #endif /* __TEST__ */
